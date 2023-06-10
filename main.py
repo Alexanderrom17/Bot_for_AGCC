6624351f675a8b948b4b0e65c92f9e3ce09769a1
Alexanderrom17 <136053386+Alexanderrom17@users.noreply.github.com>
2023-06-10 09:05:35 +0900

    Добавлен переход по нажатию кнопки в следующие кнопки (дерево проекта)

--- main.py
+++ main.py
@@ -18,29 +18,48 @@ def welcome(message):
 def user_text(message):
     if message.text.lower() == 'привет':
         markup = types.InlineKeyboardMarkup(row_width=2,)
-        item1 = types.InlineKeyboardButton(text="1251", callback_data='1251')
-        item2 = types.InlineKeyboardButton(text="1354", callback_data='1354')
+        item1 = types.InlineKeyboardButton(text="1251", callback_data='open_new_buttons_1251')
+        item2 = types.InlineKeyboardButton(text="1354", callback_data='open_new_buttons_1354')
         markup.add(item1, item2)
         bot.send_message(message.chat.id, f"Введи номер титула по которому хочешь получить СОП", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, f'Для начала работы введи "Привет"')
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.message:
-        if call.data == '1251':
-            with open('Instruction.txt', 'rb') as file:
-                bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
-                # bot.send_message(call.message.chat.id, 'Здесь будет инструкция')
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text="Перед проведением работ внимательно изучи инструкцию", reply_markup=None)
-        elif call.data == '1354':
-            with open('Копия Проверка знаний ТЭСС.xlsx', 'rb') as file:
-                bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
-                # bot.send_message(call.message.chat.id, 'Бывает')
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text="Перед проведением работ внимательно изучи инструкцию", reply_markup=None)
+@bot.callback_query_handler(func=lambda call: call.data == 'open_new_buttons_1251')
+def handle_button_click(call):
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text='Кнопка 1', callback_data='button1_clicked')
+    button2 = types.InlineKeyboardButton(text='Кнопка 2', callback_data='button2_clicked')
+    keyboard.add(button1, button2)
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          text="Перед проведением работ внимательно изучи инструкцию", reply_markup=keyboard)
 
+
+@bot.callback_query_handler(func=lambda call: call.data == 'button1_clicked')
+def handle_button1_click(call):
+    bot.answer_callback_query(call.id, text='Нажата кнопка 1')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'button1_clicked')
+def handle_button1_click(call):
+    bot.answer_callback_query(call.id, text='Нажата кнопка 1')
+
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_inline(call):
+#     if call.message:
+#         if call.data == '1251':
+#             with open('Instruction.txt', 'rb') as file:
+#                 bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
+#                 bot.send_message(call.message.chat.id, 'Здесь будет инструкция')
+                # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                #                       text="Перед проведением работ внимательно изучи инструкцию", reply_markup=None)
+        # elif call.data == '1354':
+        #     with open('Копия Проверка знаний ТЭСС.xlsx', 'rb') as file:
+        #         bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
+        #         bot.send_message(call.message.chat.id, 'Бывает')
+                # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                #                       text="Перед проведением работ внимательно изучи инструкцию", reply_markup=None)
+#
 
 bot.polling(none_stop=True)
