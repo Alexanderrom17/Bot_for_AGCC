f2da964dd3b58c17fd08bb845703f1aff5cfb23a
Alexanderrom17 <136053386+Alexanderrom17@users.noreply.github.com>
2023-06-10 22:18:03 +0900

    Без серьезных изменений. Упорядочены функции.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def user_text(message):
     if message.text.lower() == 'привет':
-        markup = types.InlineKeyboardMarkup(row_width=2)
+        markup = types.InlineKeyboardMarkup(row_width=7)
         item1 = types.InlineKeyboardButton(text="1251", callback_data='open_new_buttons_1251')
         item2 = types.InlineKeyboardButton(text="1354", callback_data='open_new_buttons_1354')
         markup.add(item1, item2)
@@ -39,6 +39,18 @@ def handle_button_click(call):
                           text="Перед проведением работ внимательно изучи инструкцию", reply_markup=keyboard)
 
 
+@bot.callback_query_handler(func=lambda call: call.data == 'button1_1251_clicked')
+def handle_button1_click(call):
+    bot.answer_callback_query(call.id, text='Нажата кнопка 1')
+    with open('Instruction.txt', 'rb') as file:
+        bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'button2_1251_clicked')
+def handle_button2_click(call):
+    bot.answer_callback_query(call.id, text='Нажата кнопка 2')
+
+
 @bot.callback_query_handler(func=lambda call: call.data == 'open_new_buttons_1354')
 def handle_button_click(call):
     keyboard = types.InlineKeyboardMarkup()
@@ -52,14 +64,12 @@ def handle_button_click(call):
                           text="Перед проведением работ внимательно изучи инструкцию", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'button1_1251_clicked')
+@bot.callback_query_handler(func=lambda call: call.data == 'button1_1354_clicked')
 def handle_button1_click(call):
     bot.answer_callback_query(call.id, text='Нажата кнопка 1')
-    with open('Instruction.txt', 'rb') as file:
-        bot.send_document(chat_id=call.message.chat.id, data=file, document=file)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'button2_clicked')
+@bot.callback_query_handler(func=lambda call: call.data == 'button2_1354_clicked')
 def handle_button2_click(call):
     bot.answer_callback_query(call.id, text='Нажата кнопка 2')
 
@@ -90,5 +100,6 @@ def handle_button_back(call):
                 # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                 #                       text="Перед проведением работ внимательно изучи инструкцию", reply_markup=None)
 #
+
 
 bot.polling(none_stop=True)
